f5b35a9980db91b8be5b0ad56c60b90e21d1557f
Admin <lloyd.trevarthen@neptune-software.com>
2024-04-05 15:18:06 +0100

    Init

---
+++ artifacts/Script/MyGlobalFunctionsExamples/content.js
@@ -0,0 +1,7 @@
+const square = async function (x) {
+    return x**2;
+};
+
+const message = async (x) => x;
+
+complete({ square, message });
---
+++ artifacts/Script/UseTheGlobalFunctions/content.js
@@ -0,0 +1,4 @@
+const GlobalLib = globals.MyGlobalFunctionsExamples // importing and shortening name
+
+console.log( await GlobalLib.square(5) );
+console.log( await GlobalLib.message("Hello Neptune!") );
